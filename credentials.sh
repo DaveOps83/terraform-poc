93efe1fd0397c553afea42b91688f5390d5cb47c
David Opperman <david.opperman@travcorpuk.com>
2016-01-27 11:21:15 +0000

    Adding support for Artifactory credentials.

--- credentials.sh
+++ credentials.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#Export Terraform environment variables for AWS credentials, called by wrapper.sh
+#Export Terraform environment variables for AWS and Artifactory credentials, called by wrapper.sh
 #Fill in your credentials for the appropriate environment you are working on.
 #When Terraform can use AWS CLI profiles this script will be removed.
 #This repository's index has been updated to ignore changes to this file:
 #git update-index --assume-unchanged credentials.sh
 #DO NOT PUSH API KEYS TO GITHUB!!!!
+export TF_VAR_artifactory_user=""
+export TF_VAR_artifactory_password=""
 case $1 in
   dev)
     export TF_VAR_aws_access_key=""
@@ -26,5 +28,9 @@ esac
 if [[ -z $TF_VAR_aws_access_key || -z $TF_VAR_aws_secret_key ]] ;
 then
   echo -e "Please set API credentials for $1 in credentials.sh"
+  exit 1
+elif [[ -z $TF_VAR_artifactory_user || -z $TF_VAR_artifactory_password ]] ;
+then
+  echo -e "Please set Artifactory credentials in credentials.sh"
   exit 1
 fi
